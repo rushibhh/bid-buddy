db412b89d53207491cdaa1a02a380cb9cffa983e
Rushabh sutariya <sutariyarushabh2@gmail.com>
2025-01-08 14:30:25 +0530

    Add in nextAuth and items table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,38 @@
-export const dynamic = "force-dynamic";
+import { auth } from "@/auth";
+import SignIn from "@/components/sign-in";
+import { SignOut } from "@/components/signout-button";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { database } from "@/db/database";
+import { items } from "@/db/schema";
+import { revalidatePath } from "next/cache";
 
-export default function Home() {
-  console.log("run code");
-  return <div>Generated value is {Math.random()}</div>;
+export default async function HomePage() {
+  const allItems = await database.query.items.findMany();
+  const session = await auth();
+  return (
+    <main className="container mx-auto py-10">
+      {session ? <SignOut /> : <SignIn />}
+      <form
+        action={async (formData: FormData) => {
+          "use server";
+          const data = formData.get("name") as string;
+          console.log(data);
+          await database.insert(items).values({
+            name: formData.get("name") as string,
+            // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+            userId: session?.user?.id!,
+          });
+          revalidatePath("/");
+        }}
+      >
+        <Input name="name" placeholder="Enter your item" />
+        <Button type="submit">Post Item</Button>
+      </form>
+
+      {allItems.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </main>
+  );
 }
---
+++ src/auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import Google from "next-auth/providers/google";
+import { database } from "./db/database";
+import { accounts, sessions, users, verificationTokens } from "./db/schema";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(database, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
+  providers: [Google],
+});
